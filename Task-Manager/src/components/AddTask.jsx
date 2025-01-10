4d162a1be0becfc828ae55ee364b008303478a9c
Thamarai <tmanalan710@gmail.com>
2025-01-10 21:23:40 +0530

    -

--- Task-Manager/src/components/AddTask.jsx
+++ Task-Manager/src/components/AddTask.jsx
@@ -19,10 +19,10 @@ export default function AddTask() {
     }
   return (
     <>
-    <div className="">
+    <div className="w-80 bg-slate-500 rounded-xl">
       <div className="py-5">
 
-      <label htmlFor="task" className="text-lg">Enter the task </label>
+      <label htmlFor="task" className="text-lg ">Enter the task </label>
       </div>
 
       <div className="py-5">
@@ -31,7 +31,7 @@ export default function AddTask() {
         placeholder="Enter the task"
         onChange={handleChange}
         value={taskAtom}
-        className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+        className="py-3 px-4 block border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
       />
 
 
