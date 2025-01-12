bb6cb3dc0fd3b73ddbe9b11e0ff8fc3bbab15e7b
Thamarai <tmanalan710@gmail.com>
2025-01-12 14:15:57 +0530

    Improving the tailwind

--- Task-Manager/src/components/AddTask.jsx
+++ Task-Manager/src/components/AddTask.jsx
@@ -1,44 +1,42 @@
 import { useRecoilState, useSetRecoilState } from "recoil";
-import { taskAtoms,taskListAtoms} from "../store/atoms";
-
+import { taskAtoms, taskListAtoms } from "../store/atoms";
 
 export default function AddTask() {
   const [taskAtom, setTaskAtom] = useRecoilState(taskAtoms);
-  const setTaskListAtom = useSetRecoilState(taskListAtoms)
+  const setTaskListAtom = useSetRecoilState(taskListAtoms);
 
-    const handleChange = (event) => {
-        setTaskAtom(event.target.value)
-        
+  const handleChange = (event) => {
+    setTaskAtom(event.target.value);
+  };
+  function addTasks() {
+    if (taskAtom.trim() != "") {
+      setTaskListAtom((oldList) => [...oldList, taskAtom]);
+      setTaskAtom("");
     }
-    function addTasks(){
-        if(taskAtom.trim() != ''){
-            setTaskListAtom((oldList)=>[...oldList,taskAtom])
-            setTaskAtom('')
-        }   
-        
-    }
+  }
   return (
     <>
-    <div className="w-80 bg-slate-500 rounded-xl">
-      <div className="py-5">
+      <div className="w-96 h-96 bg-gray-400 ml-auto mr-auto flex flex-col items-center">
+        <div className="pt-12 pb-6">
+          <label htmlFor="task" className="">
+            Enter the task{" "}
+          </label>
+        </div>
 
-      <label htmlFor="task" className="text-lg ">Enter the task </label>
-      </div>
-
-      <div className="py-5">
-      <input
-        type="text"
-        placeholder="Enter the task"
-        onChange={handleChange}
-        value={taskAtom}
-        className="py-3 px-4 block border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-      />
-
-
-      </div>
-      <div className="py-5">
-      <button onClick={addTasks} className="bg-slate-400 rounded-full w-32 py-3">Add Task</button>
-      </div>
+        <div className="pb-6">
+          <input
+            type="text"
+            placeholder="Enter the task"
+            onChange={handleChange}
+            value={taskAtom}
+            className="w-64 h-9 pl-2 border-gray-400 border-3"
+          />
+        </div>
+        <div className="">
+          <button onClick={addTasks} className="bg-white pl-6 pr-6 m-4 h-10 rounded-sm hover:bg-gray-300 border-black hover:border-2">
+            Add Task
+          </button>
+        </div>
       </div>
     </>
   );
