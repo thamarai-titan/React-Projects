0e57732e68b87024c569cb76f3b9720a15b7570f
Thamarai <tmanalan710@gmail.com>
2025-01-17 20:42:46 +0530

    working

--- amazon-cart/src/components/ShoppingCart.jsx
+++ amazon-cart/src/components/ShoppingCart.jsx
@@ -1,8 +1,20 @@
 const ShoppingCart = () => {
     return <>
     
+    <div>
 
-        
+        <h1>Shopping Cart</h1>
+        <div>
+            <img src="" alt="" />
+            <h2></h2>
+            <button></button>
+            <p></p>
+            <button></button>
+            <p></p>
+        </div>
+
+    </div>
     
     </>
 }
+export default ShoppingCart
---
+++ amazon-cart/src/components/WishList.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react"
+
+const WishList = () => {
+
+
+    useEffect(()=>{
+        const FetchingProducts = async () => {
+            try{
+                const response = axios.get(`https://fakestoreapi.com/products/${csc}`)
+                
+                
+            }
+            catch{
+                console.error(`This ${error} occured`)
+            }
+        }
+        FetchingProducts()
+    },[])
+
+
+    return <>
+    
+    <div>
+
+    <div>
+        <h2>Your Wish List</h2>
+        <p>public</p>
+        <span></span>
+        <span></span>
+        <div>
+            <img src="" alt="" />
+            <p></p>
+            <p></p>
+            <button>Add to Cart</button>
+        </div>
+    </div>
+    </div>
+    
+    
+    </>
+}
+export default WishList
